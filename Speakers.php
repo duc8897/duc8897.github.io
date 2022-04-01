236947cebb4d479528ab266fccc79bd2470de63a
Huu-Duc Huynh <69384103+duc8897@users.noreply.github.com>
2022-04-01 15:13:25 +0700

    Update Speakers.php

--- Speakers.php
+++ Speakers.php
@@ -1,1 +1,22 @@
+<html>
 
+<body>
+  
+  <form>
+    <div class="form-group">
+      <label for="SpeakerFullName">Họ và tên</label>
+      <input type="text" class="form-control" id="SpeakerFullName" placeholder="Họ và tên diễn giả">
+    </div>
+    <div class="form-group">
+      <label for="SpeakerFullName">Công ty/ Đơn vị</label>
+      <input type="text" class="form-control" id="SpeakerFullName" placeholder="Nơi diễn giả đang công tác">
+    </div>
+    <div class="form-group">
+      <label for="SpeakerPosition">Another label</label>
+      <input type="text" class="form-control" id="SpeakerPosition" placeholder="Vị trí của diễn giả ở công ty">
+    </div>
+  </form>
+  
+</body>
+  
+</html>
